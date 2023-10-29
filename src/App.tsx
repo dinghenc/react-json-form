d757cd05a6335e30e9a03f0e2503aafe33ce3990
dinghenc <308024784@qq.com>
2023-10-29 23:17:14 +0800

    feat: antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,100 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
 
-import FormRender, { useForm } from 'form-render';
+import FormRender, {useForm} from 'form-render';
+import {Button, Divider, Flex, Space, Typography} from 'antd';
 import schema from './schema/simple';
 import detail from "./detail";
+import {AlertTwoTone, BugTwoTone, BulbTwoTone} from "@ant-design/icons";
 
 export default () => {
-  const form = useForm();
+    const form = useForm();
 
-  const onFinish = (formData: any) => {
-    console.log('formData:', formData);
-  };
+    const onFinish = (formData: any) => {
+        console.log('formData:', formData);
+    };
 
-  const [posts, setPosts] = useState('123');
-  const [requestSchema, setRequestSchema] = useState(schema)
+    const [posts, setPosts] = useState('123');
+    const [requestSchema, setRequestSchema] = useState(schema)
 
-  useEffect(() => {
-    fetch('http://localhost:8999/', {
-      method: 'GET'
-    })
-        .then((res) => res.text())
-        .then((data) => {
-          console.log(data);
-          setPosts(data);
-          schema['properties']['fr-lbvk']['items']['properties']['fr-ft3a']['props']['options'][0]['label'] = data;
-          setRequestSchema(schema);
-          form.setValues({'fr-lbvk': [{'fr-ft3a': 'B', 'fr-9i0a': {'fr-j82x': [{'fr-gdsv': 'A'}]}}, {'fr-ft3a': 'A'}]})
+    useEffect(() => {
+        fetch('http://localhost:8999/', {
+            method: 'GET'
         })
-        .catch((err) => {
-          console.log(err.message);
-        });
-  }, []);
+            .then((res) => res.text())
+            .then((data) => {
+                console.log(data);
+                setPosts(data);
+                schema['properties']['fr-lbvk']['items']['properties']['fr-ft3a']['props']['options'][0]['label'] = data;
+                setRequestSchema(schema);
+                form.setValues({
+                    'fr-lbvk': [{
+                        'fr-ft3a': 'B',
+                        'fr-9i0a': {'fr-j82x': [{'fr-gdsv': 'A'}]}
+                    }, {'fr-ft3a': 'A'}]
+                })
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
 
-  return (
-      <div style={{width: "50%", textAlign:"center", margin: "auto"}}>
-        <h1>This is a simple form system</h1>
-        <pre>
-          {posts}
-        </pre>
-        <br/>
-        <h2>1. Next is some detail</h2>
-        <div style={{textAlign:"left", }}>
-          <div style={{borderStyle:"double"}}>
-            <pre>{detail}</pre>
-          </div>
+    const rowStyle = {
+        display: "flex",
+        justifyContent:"center"
+    };
+
+    return (
+        <div style={{width: "50%", textAlign: "center", margin: "auto"}}>
+            <div style={rowStyle}>
+                <h1><BulbTwoTone />&nbsp;</h1>
+                <h1>This is a simple form system</h1>
+            </div>
+            <pre>
+              {posts}
+            </pre>
+            <br/>
+            <div>
+                <div style={{width: "100%"}}>
+                    <Divider orientation="left">
+                        <div style={rowStyle}>
+                            <h2><AlertTwoTone />&nbsp;</h2>
+                            <h2>1. Next is some detail</h2>
+                        </div>
+                    </Divider>
+
+                    <div style={{textAlign: "left"}}>
+                        <Typography>
+                            <pre>{detail}</pre>
+                        </Typography>
+                        <div style={{textAlign: "center"}}>
+                            <Space wrap>
+                                <Button type="primary" shape="circle">👍</Button>
+                                <Button type="primary" shape="circle" danger>👎</Button>
+                            </Space>
+                        </div>
+                    </div>
+                    <br/>
+                </div>
+                <div style={{width: "100%"}}>
+                    <Divider orientation="left">
+                        <div style={rowStyle}>
+                            <h2><BugTwoTone />&nbsp;</h2>
+                            <h2>2. Next is some select</h2>
+                        </div>
+                    </Divider>
+                    <div style={{textAlign: "left",}}>
+                        <FormRender
+                            form={form}
+                            schema={requestSchema}
+                            onFinish={onFinish}
+                            maxWidth={360}
+                            footer={true}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-        <br/>
-        <h2>2. Next is some select</h2>
-        <div style={{textAlign:"left", }}>
-          <FormRender
-              form={form}
-              schema={requestSchema}
-              onFinish={onFinish}
-              maxWidth={360}
-              footer={true}
-          />
-        </div>
-      </div>
-  );
+    );
 }
--- src/schema/simple.ts
+++ src/schema/simple.ts
@@ -3,8 +3,8 @@ let jsonStr = `{
   "properties": {
     "fr-lbvk": {
       "title": "Step List",
-      "description": "This is a step list",
       "type": "array",
+      "description": "This is a step list",
       "items": {
         "title": "Step",
         "description": "This is a step",
@@ -14,7 +14,9 @@ let jsonStr = `{
           "fr-ft3a": {
             "title": "Step Name",
             "type": "string",
+            "widget": "select",
             "props": {
+              "placeholder": "select a step",
               "options": [
                 {
                   "label": "First Step",
@@ -25,21 +27,20 @@ let jsonStr = `{
                   "value": "B"
                 }
               ],
-              "placeholder": "select a step"
-            },
-            "widget": "select"
+              "mode": "single"
+            }
           },
           "fr-9i0a": {
             "title": "Sub Step List",
-            "description": "This is a sub step list",
             "column": 1,
             "type": "object",
+            "description": "This is a sub step list",
             "widget": "collapse",
             "properties": {
               "fr-j82x": {
                 "title": "Sub Step",
-                "description": "This is a sub step",
                 "type": "array",
+                "description": "This is a sub step",
                 "items": {
                   "title": "Sub Step",
                   "description": "This is a sub step",
@@ -49,7 +50,9 @@ let jsonStr = `{
                     "fr-gdsv": {
                       "title": "Sub Step Name",
                       "type": "string",
+                      "widget": "select",
                       "props": {
+                        "placeholder": "select a sub step",
                         "options": [
                           {
                             "label": "First Sub Step",
@@ -60,9 +63,8 @@ let jsonStr = `{
                             "value": "B"
                           }
                         ],
-                        "placeholder": "select a sub step"
-                      },
-                      "widget": "select"
+                        "mode": "single"
+                      }
                     }
                   }
                 }
@@ -71,11 +73,26 @@ let jsonStr = `{
           }
         }
       }
+    },
+    "fr-5m89": {
+      "title": "Input 1",
+      "type": "string",
+      "widget": "input"
+    },
+    "fr-dpny": {
+      "title": "Input 2",
+      "type": "string",
+      "widget": "input"
+    },
+    "fr-7of6": {
+      "title": "Input 3",
+      "type": "string",
+      "widget": "input"
     }
   },
-  "displayType": "column",
-  "maxWidth": "1000px",
-  "column": 1
+  "displayType": "row",
+  "layout": "vertical",
+  "maxWidth": "340px"
 }`;
 
 export default JSON.parse(jsonStr);
